1d401334ca09ac041f567aff26a1b047af32e8e0
antonlazurko <lazurkoanton87@gmail.com>
2020-07-23 15:33:51 +0300

    делает 2,3,4 задачи

--- index.js
+++ index.js
@@ -1,53 +1,67 @@
 //--------------task #1
-const name = '«Генератор защитного поля»';
-let price = 1000;
-const firstMessage = `Выбран ${name}, цена за штуку ${price} кредитов`;
-price = 2000;
-secondMessage = `'Выбран' ${name}, 'цена за штуку' ${price} кредитов`;
+// const name = '«Генератор защитного поля»';
+// let price = 1000;
+// const firstMessage = `Выбран ${name}, цена за штуку ${price} кредитов`;
+// price = 2000;
+// secondMessage = `'Выбран' ${name}, 'цена за штуку' ${price} кредитов`;
 
-console.log(firstMessage);
+// console.log(firstMessage);
 
-console.log(secondMessage);
+// console.log(secondMessage);
 //-------------task#2
-const invoice = 100;
-const stock = 100;
+// const invoice = 100;
+// const stock = 100;
 
-const message =
-  stock <= invoice
-    ? 'Заказ оформлен, с вами свяжется менеджер'
-    : 'На складе недостаточно товаров!';
+// const message =
+//   invoice > stock
+//     ? 'Заказ оформлен, с вами свяжется менеджер'
+//     : 'На складе недостаточно товаров!';
 
-console.log(message);
+// console.log(message);
+//-------------task#3
+// const userPassword = 'jqueryismyjam';
+// const ADMIN_PASSWORD = 'jqueryismyjam';
+// const CANCELED_BY_USER = 'Отменено пользователем!';
+// const ACCESS_IS_ALLOWED = 'Добро пожаловать!';
+// const ACCESS_DENIED = 'Доступ запрещен, неверный пароль!';
+// let message;
+// if (userPassword === null) {
+//   message = CANCELED_BY_USER;
+// } else if (userPassword !== ADMIN_PASSWORD) {
+//   message = ACCESS_DENIED;
+// } else if (userPassword === ADMIN_PASSWORD) {
+//   message = ACCESS_IS_ALLOWED;
+// }
+// console.log(message);
+//--------------task#4
+// const orderPieces = 4;
 
-let cost;
-const subscription = 'premium';
+// const credits = 23580;
+// const pricePerDroid = 3000;
+// const CANCELED_BY_USER = 'Отменено пользователем!';
+// const ACCESS_DENIED = 'Недостаточно средств на счету!';
 
-switch (subscription) {
-  case 'free':
-    cost = 0;
-    break;
-
-  case 'pro':
-    cost = 100;
-    break;
-
-  case 'premium':
-    cost = 500;
-    break;
-
-  default:
-    console.log('Invalid subscription type');
-}
-
-console.log(cost);
-//если invoice равен 100
-// то значение message будет равно
-// 'Заказ оформлен, с вами свяжется менеджер'
-
-//если invoice равен 50
-// то значение message будет равно
-// 'Заказ оформлен, с вами свяжется менеджер'
-
-//если invoice равен 150
-// то значение message будет равно
-// 'На складе недостаточно товаров!'
+// let totalPrice; // Write code in this line
+// let balanceСredit; // Write code in this line
+// let message;
+// if (orderPieces === null) {
+//   message = CANCELED_BY_USER;
+// } else if (orderPieces === 0) {
+//   balanceСredit = credits - pricePerDroid * orderPieces;
+//   message = `Вы купили ${orderPieces} дроидов, на счету осталось ${balanceСredit} кредитов`;
+// } else if (orderPieces === 1) {
+//   balanceСredit = credits - pricePerDroid * orderPieces;
+//   message = `Вы купили ${orderPieces} дроидов, на счету осталось ${balanceСredit} кредитов`;
+// } else if (orderPieces === 4) {
+//   balanceСredit = credits - pricePerDroid * orderPieces;
+//   message = `Вы купили ${orderPieces} дроидов, на счету осталось ${balanceСredit} кредитов`;
+// } else if (orderPieces === 7) {
+//   balanceСredit = credits - pricePerDroid * orderPieces;
+//   message = `Вы купили ${orderPieces} дроидов, на счету осталось ${balanceСredit} кредитов`;
+// } else if (orderPieces === 8) {
+//   message = ACCESS_DENIED;
+// } else if (orderPieces === 12) {
+//   message = ACCESS_DENIED;
+// }
+// console.log(message);
+//---------------task#5

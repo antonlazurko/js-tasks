b6d1ebe73a178fa91c77ae0bb60e30903f1cdbeb
antonlazurko <lazurkoanton87@gmail.com>
2020-08-03 22:19:37 +0300

    делает задача 1 модуль 3

--- extraTasks.js
+++ extraTasks.js
@@ -35,15 +35,11 @@
 //     let str = num.split('');
 //     let result = [str[0]];
 //     for (let i = 1; i < str.length; i += 1) {
-//         //     if (str[i - 1] % 2 !== 0 && str[i] % 2 !== 0) {
-//         //         result.push(':', str[i]);
-//         //     } else {
-//         //         result.push(str[i]);
-//         //     }
-//         result =
-//             str[i - 1] % 2 !== 0 && str[i] % 2 !== 0
-//                 ? result.push(':', str[i])
-//                 : result.push(str[i]);
+//         if (str[i - 1] % 2 !== 0 && str[i] % 2 !== 0) {
+//             result.push(':', str[i]);
+//         } else {
+//             result.push(str[i]);
+//         }
 //     }
 //     return result.join('');
 // }

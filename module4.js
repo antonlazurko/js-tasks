17b6a4f662be506f66e4ca7aed4e41515770af09
antonlazurko <lazurkoanton87@gmail.com>
2020-08-20 10:20:07 +0300

    делает модуль 4 задача 5

--- module4.js
+++ module4.js
@@ -1,4 +1,4 @@
-// ---------------------------task1
+'use strict'; // ---------------------------task1
 // Callback функция
 // Функция mapArray(array, cb), принимает 1 - м параметром array - массив чисел, а вторым параметром cb - функцию обратного вызова(callback).Функция mapArray создает новый массив numbers и заполняет его числами из массива array преобразованными функцией cb.
 
@@ -139,3 +139,52 @@
 
 // console.log(account.showOrders());
 // ['order-1', 'order-2', 'order-3', 'order-4']
+// 'use strict';
+
+//-------------------task5
+//bind для замены this в методах объекта
+// Оформи вызов метода invokeInventoryAction таким образом, чтобы в качестве this для методов
+// inventory.add
+// inventory.remove выступал объект inventory
+// const inventory = {
+//     items: ['Knife', 'Gas mask'],
+//     add(itemName) {
+//         this.items.push(itemName);
+//         return `Adding ${itemName} to inventory`;
+//     },
+//     remove(itemName) {
+//         this.items = this.items.filter(item => item !== itemName);
+//         return `Removing ${itemName} from inventory`;
+//     },
+// };
+
+// const invokeInventoryAction = function (itemName, action) {
+//     const act = action(itemName);
+//     const msg = `Invoking action on ${itemName}`;
+//     return { act, msg };
+// };
+
+// const invokeAdd = invokeInventoryAction(
+//     'Medkit',
+//     inventory.add.bind(inventory), // Write code in this line
+// );
+// const arrayAdd = [...inventory.items];
+
+// console.log(invokeAdd);
+// //{ act: 'Adding Medkit to inventory', msg: 'Invoking action on Medkit' }
+
+// console.log(arrayAdd);
+// // ['Knife', 'Gas mask', 'Medkit']
+
+// const invokeRemove = invokeInventoryAction(
+//     'Gas mask',
+//     inventory.remove.bind(inventory), // Write code in this line
+// );
+
+// const arrayRemove = [...inventory.items];
+
+// console.log(invokeRemove);
+// //{ act: 'Removing Gas mask from inventory', msg: 'Invoking action on Gas mask' }
+
+// console.log(arrayRemove);
+// // ['Knife', 'Medkit']
